013c789f8da07563361d1bb3920309037005d530
musabd96 <mustafa.abdulle@student.nbi-handelsakademin.se>
2023-05-13 13:36:34 +0200

    Booking page create and link to navbar

---
+++ RideOn/Controllers/BookingController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RideOn.Data;
+
+namespace RideOn.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: Cars
+        public async Task<IActionResult> Index()
+        {
+            return _context.Car != null ?
+                        View(await _context.Car.ToListAsync()) :
+                        Problem("Entity set 'ApplicationDbContext.Car'  is null.");
+        }
+
+        // GET: Cars/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Car == null)
+            {
+                return NotFound();
+            }
+
+            var car = await _context.Car
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            return View(car);
+        }
+    }
+
+}
