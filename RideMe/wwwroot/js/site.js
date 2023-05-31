00bd900a37b7b56cbdbcbc1b7277aa11e016c6c3
musabd96 <mustafa.abdulle@student.nbi-handelsakademin.se>
2023-05-31 19:17:55 +0200

    fix to book todays date and return start day after the pickup date

--- RideMe/wwwroot/js/site.js
+++ RideMe/wwwroot/js/site.js
@@ -54,3 +54,46 @@ document.getElementById("close-popup-btn").addEventListener("click", function ()
     document.querySelector(".popup-overlay").style.display = "none";
 
 });
+
+
+// Get today's date
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+var yyyy = today.getFullYear();
+
+// Format it as yyyy-mm-dd for the input's value
+var todayDate = yyyy + '-' + mm + '-' + dd;
+
+// Set the minimum value of the date input to today's date
+document.querySelector('.pickup-date').setAttribute('min', todayDate);
+document.querySelector('.pickup-date-popup').setAttribute('min', todayDate);
+
+
+var pickupDateInput = document.querySelector('.pickup-date');
+var pickupDatePopupInput = document.querySelector('.pickup-date-popup');
+var returnDateInput = document.querySelector('.return-date');
+var returnDatePopupInput = document.querySelector('.return-date-popup');
+
+// Add an event listener to the Pickup Date input
+pickupDateInput.addEventListener('change', function () {
+    var selectedDate = new Date(pickupDateInput.value);
+    selectedDate.setDate(selectedDate.getDate() + 1);
+
+    // Format the minimum date for the Return Date input
+    var minDate = selectedDate.toISOString().split('T')[0];
+
+    // Set the minimum value of the Return Date input
+    returnDateInput.setAttribute('min', minDate);
+});
+
+pickupDatePopupInput.addEventListener('change', function () {
+    var selectedDate = new Date(pickupDatePopupInput.value);
+    selectedDate.setDate(selectedDate.getDate() + 1);
+
+    // Format the minimum date for the Return Date input
+    var minDate = selectedDate.toISOString().split('T')[0];
+
+    // Set the minimum value of the Return Date input
+    returnDatePopupInput.setAttribute('min', minDate);
+});
